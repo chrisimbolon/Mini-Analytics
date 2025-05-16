791f69b55f5308ec34fe1b2d35165049b91fba7c
Christyan Simbolon <sambungdata@gmail.com>
2025-05-16 14:58:33 +0700

    attempting to integrate frontend and backend-attempts fourteen

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,4 @@
 
-// const API_BASE = "http://localhost:8000";
 // const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 const API_BASE = import.meta.env.VITE_API_URL || "/mini-analytics/api";
